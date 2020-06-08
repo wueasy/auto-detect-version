843ba670f7e7641b5f641c39c5bfd68294aaf9ff
fallsea <240312387@qq.com>
2020-06-08 19:14:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,120 @@
+# auto-detect-version
+
+·auto-detect-version· 是一个用于自定检测pom文件中所依赖jar包最新版本的工具，用于快速检测版本中maven中心仓库中的最小版本。
+
+当发现pom文件中的版本号和maven中心长裤不一致时，输出最新的相关信息。
+
+
+## 特性
+
+* 检测最新版本
+* 检测大版本下的最新版本
+
+## 使用教程
+
+直接启用服务，通过sheel命令行方式启动。
+
+`check E:\\git\\aliyun\\wueasy\\pom.xml`
+
+* `check` 命名
+* `E:\\git\\aliyun\\wueasy\\pom.xml` 为参数，可以是本地的pom文件绝对路径，或者http地址
+
+
+
+## 执行效果
+
+* `latest`：最新版本
+* `largeLatest`：大版本下的最新版本
+
+```
+shell:>check E:\\git\\aliyun\\wueasy\\pom.xml
+<dependency>
+    <groupId>mysql</groupId>
+    <artifactId>mysql-connector-java</artifactId>
+    <version>5.1.49</version>
+    <latest>8.0.20</latest>
+    <largeLatest>5.1.49</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>org.apache.curator</groupId>
+    <artifactId>curator-framework</artifactId>
+    <version>4.3.0</version>
+    <latest>5.0.0</latest>
+    <largeLatest>4.3.0</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>org.apache.curator</groupId>
+    <artifactId>curator-client</artifactId>
+    <version>4.3.0</version>
+    <latest>5.0.0</latest>
+    <largeLatest>4.3.0</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>org.apache.curator</groupId>
+    <artifactId>curator-recipes</artifactId>
+    <version>4.3.0</version>
+    <latest>5.0.0</latest>
+    <largeLatest>4.3.0</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>com.squareup.okhttp3</groupId>
+    <artifactId>okhttp</artifactId>
+    <version>3.14.9</version>
+    <latest>4.7.2</latest>
+    <largeLatest>3.14.9</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>org.apache.zookeeper</groupId>
+    <artifactId>zookeeper</artifactId>
+    <version>3.5.8</version>
+    <latest>3.6.1</latest>
+    <largeLatest>3.6.1</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>com.aliyun.oss</groupId>
+    <artifactId>aliyun-sdk-oss</artifactId>
+    <version>3.9.1</version>
+    <latest>3.10.0</latest>
+    <largeLatest>3.10.0</largeLatest>
+</dependency>
+
+<dependency>
+    <groupId>com.huaweicloud</groupId>
+    <artifactId>esdk-obs-java</artifactId>
+    <version>3.19.7</version>
+    <latest>3.20.4.2</latest>
+    <largeLatest>3.20.4.2</largeLatest>
+</dependency>
+
+检测成功！
+shell:>
+```
+
+
+
+## License
+
+```java
+/*
+ * auto-detect-version - Tools for automatically detecting the latest version of dependent packages in POM files.
+ * Copyright (C) 2017-2020 wueasy.com , All Rights Reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+```
---
+++ auto-detect-version/src/main/java/com/wueasy/autodetectversion/commands/CheckCommands.java
@@ -0,0 +1,182 @@
+/*
+ * auto-detect-version - Tools for automatically detecting the latest version of dependent packages in POM files.
+ * Copyright (C) 2017-2020 wueasy.com , All Rights Reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wueasy.autodetectversion.commands;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.xml.sax.SAXException;
+
+@ShellComponent
+public class CheckCommands {
+	
+	/**
+	 * 检测的地址，默认maven中心仓库
+	 */
+	private static final String CHECK_URL = "https://repo1.maven.org/maven2/";
+	
+
+	/**
+	 * check E:\\git\\aliyun\\wueasy\\pom.xml
+	 * check E:\\git\\wueasy\\auto-detect-version\\auto-detect-version\\pom.xml
+	 * @author: fallsea
+	 * @param path
+	 * @return
+	 */
+	@ShellMethod(value = "检测pom文件中的最新jar包依赖版本", key = "check")
+    public String check(String path){
+		
+		SAXReader reader = new SAXReader();
+		reader.setValidation(false);
+		try {
+			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		} catch (SAXException e) {
+			return "读取pom文件失败！";
+		}
+		Document document = null;
+		try {
+			if(path.startsWith("http://") || path.startsWith("https://")) {
+				document = reader.read(new URL(path));
+			}else {
+				document = reader.read(new File(path));
+			}
+		} catch (DocumentException e) {
+			return "读取pom文件失败！";
+		} catch (MalformedURLException e) {
+			return "读取pom文件失败！";
+		} catch (Exception e) {
+			return "读取pom文件失败！";
+		}
+		
+		Map<String,String> variableMap = new HashMap<>();
+
+		//获取配置变量信息
+		List<Element> propertieList = document.getRootElement().element("properties").elements();
+		for (Element element : propertieList) {
+			variableMap.put("${"+element.getName()+"}", element.getTextTrim());
+		}
+		
+		if(null!=document.getRootElement().element("parent")) {
+			Element element = document.getRootElement().element("parent");
+			String groupId = element.elementText("groupId");
+			String artifactId = element.elementText("artifactId");
+			String version = element.elementText("version");
+			
+			if(StringUtils.isNotEmpty(version) && version.startsWith("$")) {
+				version = variableMap.get(version);
+			}
+			if(StringUtils.isNotEmpty(version)) {
+				check(groupId, artifactId, version);
+			}
+		}
+		
+		//获取jar包信息
+		if(null!=document.getRootElement().element("dependencyManagement") && null!=document.getRootElement().element("dependencyManagement").element("dependencies")) {
+			check(document.getRootElement().element("dependencyManagement").element("dependencies").elements(), variableMap);
+		}
+		if(null!=document.getRootElement().element("dependencies")) {
+			check(document.getRootElement().element("dependencies").elements(), variableMap);
+		}
+		return "检测成功！";
+    }
+	
+	/**
+	 * 
+	 * @author: fallsea
+	 * @param dependencieList
+	 * @param variableMap
+	 */
+	private void check(List<Element> dependencieList,Map<String,String> variableMap) {
+		if(null!=dependencieList && !dependencieList.isEmpty()) {
+			for (Element element : dependencieList) {
+				String groupId = element.elementText("groupId");
+				String artifactId = element.elementText("artifactId");
+				String version = element.elementText("version");
+				
+				if(StringUtils.isNotEmpty(version) && version.startsWith("$")) {
+					version = variableMap.get(version);
+				}
+				if(StringUtils.isNotEmpty(version)) {
+					check(groupId, artifactId, version);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @author: fallsea
+	 * @param groupId
+	 * @param artifactId
+	 * @param version
+	 */
+	private void check(String groupId,String artifactId,String version) {
+		String url = CHECK_URL + groupId.replace(".", "/")+"/"+artifactId.replace(".", "/")+"/maven-metadata.xml";
+		try {
+			SAXReader reader = new SAXReader();
+			reader.setValidation(false);
+			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			Document document = reader.read(new URL(url));
+			String latest = document.getRootElement().element("versioning").elementText("release");
+			if(!version.equals(latest)) {
+				System.out.println("<dependency>");
+				System.out.println("    <groupId>"+groupId+"</groupId>");
+				System.out.println("    <artifactId>"+artifactId+"</artifactId>");
+				System.out.println("    <version>"+version+"</version>");
+				//最新版本
+				System.out.println("    <latest>"+latest+"</latest>");
+				//同版本最新版本
+				
+				if(version.indexOf(".")!=-1) {
+					String start = version.substring(0,version.indexOf(".")+1);
+					List<Element> versionList = document.getRootElement().element("versioning").element("versions").elements();
+					if(null!=versionList && !versionList.isEmpty()) {
+						String latest2 = "";
+						for (Element element : versionList) {
+							if(element.getText().startsWith(start)) {
+								latest2 = element.getText();
+							}
+						}
+						if(StringUtils.isNotEmpty(latest2)) {
+							System.out.println("    <largeLatest>"+latest2+"</largeLatest>");
+						}
+					}
+				}
+				
+				
+				
+				System.out.println("</dependency>");
+				System.out.println();
+			}
+		} catch (DocumentException e) {
+		} catch (SAXException e) {
+		} catch (MalformedURLException e) {
+		} catch(Exception e) {
+		}
+	}
+}
